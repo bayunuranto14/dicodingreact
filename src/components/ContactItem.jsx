c73401e8974c12a7e1fc90842893218564570068
bayunuranto14 <bayunuranto@gmail.com>
2023-10-01 11:35:00 +0700

    update button delete from state-component and event-handling

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -2,13 +2,15 @@
 import React from "react";
 import ContactItemBody from "./ContactItemBody";
 import ContactItemImage from "./ContactItemImage";
+import DeleteButton from "./DeleteButton";
 
 // eslint-disable-next-line react/prop-types
-function ContactItem({ imageUrl, name, tag }) {
+function ContactItem({ imageUrl, name, tag, id, onDelete }) {
   return (
     <div className="contact-item">
       <ContactItemImage imageUrl={imageUrl} />
       <ContactItemBody name={name} tag={tag} />
+      <DeleteButton id={id} onDelete={onDelete} />
     </div>
   );
 }
---
+++ src/components/DeleteButton.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function DeleteButton({ id, onDelete }) {
+  return (
+    <button className="contact_item__delete" onClick={() => onDelete(id)}>
+      X
+    </button>
+  );
+}
+
+export default DeleteButton;
